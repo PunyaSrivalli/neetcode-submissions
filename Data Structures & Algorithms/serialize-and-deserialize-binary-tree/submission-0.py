23d3c23a691070440a6c365f128a3ccce5e2855e
Punya Srivalli Nallamothu <51987096+PunyaSrivalli@users.noreply.github.com>
2026-07-28 14:11:15 -0500

    Add: serialize-and-deserialize-binary-tree - submission-0

---
+++ Data Structures & Algorithms/serialize-and-deserialize-binary-tree/submission-0.py
@@ -0,0 +1,50 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+
+class Codec:
+    
+    # Encodes a tree to a single string.
+    def serialize(self, root: Optional[TreeNode]) -> str:
+        res = []
+        def dfs(root):
+            if not root:
+                res.append("N")
+            else:
+                res.append(str(root.val))
+                dfs(root.left)
+                dfs(root.right)
+                
+            return
+        dfs(root)
+        s = ",".join(res)
+        print(s)
+        return s       
+    # Decodes your encoded data to tree.
+    def deserialize(self, data: str) -> Optional[TreeNode]:
+        values = data.split(",")
+        print(values)
+        index = 0
+        def dfs():
+            nonlocal index
+            value = values[index]
+            index += 1
+
+            if value == "N":
+                return None
+            node = TreeNode(int(value))
+            node.left = dfs()
+            node.right = dfs()
+            return node
+        return dfs()
+
+
+        
+
+
+
+
+
