60704cc104b859ddaab1b4c98fef531a3fc9a3f2
Punya Srivalli Nallamothu <51987096+PunyaSrivalli@users.noreply.github.com>
2026-08-14 11:32:04 -0500

    Add: combinations-of-a-phone-number - submission-1

---
+++ Data Structures & Algorithms/combinations-of-a-phone-number/submission-1.py
@@ -0,0 +1,24 @@
+class Solution:
+    def letterCombinations(self, digits: str) -> List[str]:
+        m = {
+            "2":["a","b","c"],
+            "3":["d","e","f"],
+            "4":["g","h","i"],
+            "5":["j","k","l"],
+            "6":["m","n","o"],
+            "7":["p","q","r","s"],
+            "8":["t","u","v"],
+            "9":["w","x","y","z"]
+        }
+        res = []
+        def dfs(i,curr):
+            if i == len(digits):
+                res.append(curr)
+                return
+            for c in m[digits[i]]:
+                dfs(i+1,curr+c)
+        if digits:
+            dfs(0,"")
+        return res
+             
+            
